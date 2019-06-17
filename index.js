c3905fa34925b26759f8a1b6041dd2215957b930
Joy Lin <vvlnote@hotmail.com>
2019-06-17 22:21:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // Code your solution in this file!
 function returnFirstTwoDrivers(drivers) {
   let newArr = [];
-  newArr.push(drivers[0]);
-  newArr.push(drivers[1]);
-  return newArr;
+  newArr.push(drivers.shift());
+  newArr.push(drivers.shift());
 }
+
+function return
